2498062bbd1646c95462013ef98120f0b72e51e0
radwanHb <radwanhbobati@gmail.com>
2022-03-24 20:19:04 +0200

    add route me

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/me', function () {
+    
+    return '<h1> hahahahaha <h1>';
+});
